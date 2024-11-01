c572d5444209a3143dc4204e7cace6a826390440
unknown <JR2353@bijisen.local>
2024-11-01 12:13:19 +0900

    ログイン（サインイン）を実装しました。※ログアウト機能も未実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,13 @@
 class HomeController < ApplicationController
+    before_action :current_user
+
+    private
+
+    def current_user
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        else
+           redirect_to new_session_path 
+        end
+    end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+    before_action :logged_in?, only: [:new, :create]
+
+    def new
+
+    end
+
+    def create
+        @user = User.find_by(s_number: params[:s_number], password: params[:password])
+        if @user
+            session[:user_id] = @user.id
+            flash[:notice] = "ログインに成功しました。"
+            redirect_to home_index_path
+        else
+            flash[:alert] = "パスワードが間違っているか、存在しないユーザーです。"
+            render action: "new"
+        end
+    end
+
+    def destroy
+        session[user_id] = nil
+        redirect_to new_sessions_path
+    end
+
+    private
+
+    def logged_in?
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+            redirect_to home_index_path
+        end
+    end
+end
