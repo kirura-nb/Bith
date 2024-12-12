10ae78e50ba66e66005211d52dfb0cf2ff3ec337
unknown <JR2353@bijisen.local>
2024-12-12 12:44:23 +0900

    ユーザー登録機能（趣味の設定まで）

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,18 @@ Rails.application.routes.draw do
   # root 'home#index'
   root :to => 'users#new'
 
-  resource :users, only: [:new, :show, :edith, :create, :update]
+  resource :users, only: [:new, :sign_up, :show, :edith, :create, :update]
 
   # ログイン機能のルーティング設定
   get 'login', to: 'sessions#new', as: 'new_sessions'
   post 'login', to: 'sessions#create', as: 'create_sessions'
   delete 'logout', to: 'sessions#destroy', as: 'destroy_sessions'
+
+  get 'users/sign_up1'
+  get 'users/sign_up2'
+  get 'users/sign_up3'
+  get 'users/sign_up4'
+  get 'users/sign_in'
 
   # ログイン後のパスを取得
   get 'home/index'
